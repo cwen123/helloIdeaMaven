a6be3573d4bc78172875631839e9d0d6ed522270
lwb <1525809014@qq.com>
2021-09-19 19:22:51 +0800

    加了一些 TODO，注意一下 java 的缩进习惯

--- src/main/java/org/example/mapper/UserMapper.java
+++ src/main/java/org/example/mapper/UserMapper.java
@@ -1,10 +1,14 @@
 package org.example.mapper;
+
 import org.example.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper //注释为Mapper
 public interface UserMapper {
+
     User getUser(int id);
+
     void addUser(User user);
+
     void delUser(int id);
 }
--- src/main/java/org/example/service/HelloService.java
+++ src/main/java/org/example/service/HelloService.java
@@ -1,8 +1,12 @@
 package org.example.service;
+
 import org.example.entity.User;
 
 public interface HelloService {
+
     User getUser(int id);
+
     void addUser(User user);
+
     void delUser(int id);
 }
